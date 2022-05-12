5d560a3592c6e94f7eacc285e598ef84dbf27a3f
Teri Lenard <teri.lenard@umfst.ro>
2022-05-12 10:27:37 +0300

    Added license and contributors in source files

--- win/src/HMAC.cpp
+++ win/src/HMAC.cpp
@@ -1,3 +1,16 @@
+/*
+* This work is licensed under the terms of the MIT license.  
+* For a copy, see <https://opensource.org/licenses/MIT>.
+*
+* Developed by NISLAB - Network and Information Security Laboratory
+* at George Emil Palade University of Medicine, Pharmacy, Science and
+* Technology of Târgu Mureş <https://nislab.umfst.ro/>
+*
+* Contributors: Teri Lenard
+*
+* Source code was taken from: https://docs.microsoft.com/en-us/windows/win32/seccrypto/example-c-program--creating-an-hmac
+*/
+
 #include "HMAC.h"
 
 
